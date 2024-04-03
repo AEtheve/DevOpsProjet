d85be2bed0dfb6b0014f95d90a4a8f714fa1e064
Alexis Etheve <alexis.etheve@etu.univ-grenoble-alpes.fr>
2024-04-03 10:38:52 +0200

    Lecture et affichage csv
    Ajout de la classe DataFrame pour la manipulation de données tabulaires
    
    La classe DataFrame permet de créer un DataFrame à partir de données en mémoire ou d'un fichier CSV. Elle contient également une méthode display() pour afficher les données du DataFrame.
    
    La méthode de construction DataFrame(String csvFilePath) permet de charger les données à partir d'un fichier CSV, en utilisant la première ligne comme noms de colonnes et les lignes suivantes comme données.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,12 @@
-package pandajava.java;
 import java.io.IOException;
-
 public class Main {
     public static void main(String[] args) {
-        // try {
-        //     DataFrame df = new DataFrame("src/main/resources/example1.csv");
-        //     df.display();
-        // } catch (Exception e) {
-        //     e.printStackTrace();
-        // }
-        System.out.println("Hello, World!");
+        try {
+            DataFrame df = new DataFrame("src/main/ressources/example1.csv");
+            df.display();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
