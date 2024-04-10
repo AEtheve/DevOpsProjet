2544fa07468a0b6c433f077f9c292321217d27db
Alexis Etheve <alexis.etheve@etu.univ-grenoble-alpes.fr>
2024-04-10 14:25:50 +0200

    Configuration maven pour ajout des tests

---
+++ src/test/java/minimal/DataFrameTest.java
@@ -0,0 +1,12 @@
+package minimal;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+
+public class DataFrameTest {
+    @Test
+    public void test1() {
+        assertTrue(true);
+    }
+}
